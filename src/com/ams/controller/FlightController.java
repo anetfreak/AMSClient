89728e1f5a66d69139cdc278d77e0c401ac87ccb
Amit Agrawal <amit.agrawal@sjsu.edu>
2013-12-07 14:35:34 -0800

    updated for List Flights

--- src/com/ams/controller/FlightController.java
+++ src/com/ams/controller/FlightController.java
@@ -1,5 +1,9 @@
 package com.ams.controller;
 
+import java.rmi.RemoteException;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +14,9 @@ import com.service.AuthenticationServiceProxy;
 import com.service.CustomerServiceProxy;
 import com.service.EmployeeServiceProxy;
 import com.service.FlightServiceProxy;
+
+import com.domain.Flight;
+import com.domain.FlightTime;
 
 @Controller
 public class FlightController {
@@ -37,6 +44,28 @@ public class FlightController {
 		System.out.println("Return Date: " + returnDate);
 		
 		return new ModelAndView("search_flight");
+	}
+	
+	@RequestMapping(value = "/ListFlight.htm", method = RequestMethod.GET)
+		public ModelAndView showFlights() {
+		
+		Flight[] flights = null;
+			
+		flightProxy.setEndpoint("http://localhost:8080/AMS/services/FlightService");
+		
+		try {
+			flights = flightProxy.getFlights();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		
+		List<Flight> flight_list = Arrays.asList(flights);
+		if(flight_list != null)
+		{
+			System.out.println("Size : "+ flight_list.size());
+		}
+		
+		return new ModelAndView("ListFlight","arr_flights", flight_list);
 	}
 	
 }
