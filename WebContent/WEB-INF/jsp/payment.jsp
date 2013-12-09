2d733cf226e6f5cc920190ecf72dbc3dcbf0f118
anetfreak <anetfreak@gmail.com>
2013-12-09 03:24:58 -0800

    Should make a reservation now.. Still work in progress..

--- WebContent/WEB-INF/jsp/payment.jsp
+++ WebContent/WEB-INF/jsp/payment.jsp
@@ -76,38 +76,6 @@ $(document).ready(function(){
 		});
     });
 });
-	function validateForm() {
-		var name = document.forms["paymentform"]["ownerName"].value;
-		var cardNumber1 = document.forms["paymentform"]["cardNumber1"].value;
-		var cardNumber2 = document.forms["paymentform"]["cardNumber2"].value;
-		var cardNumber3 = document.forms["paymentform"]["cardNumber3"].value;
-		var cardNumber4 = document.forms["paymentform"]["cardNumber4"].value;
-		var cvv = document.forms["paymentform"]["cvv"].value;
-		//var expiryDate = document.forms["paymentform"]["expiryDate"].value;
-		var expiryDate = $('#expireDate').val();
-		
-		var letters = /^[A-Za-z]+$/;
-		var numbers = /^[0-9]+$/;
-		
-		if((name == null) || (name == "") || (cardNumber1 == null) || (cardNumber1 == "") || (cardNumber2 == null)
-				|| (cardNumber2 == "") || (cardNumber3 == null) || (cardNumber3 == "") || (cardNumber4 == null) ||
-					(cardNumber4 == "") || (cvv == null) || (cvv == "") || (expiryDate == null) || (expiryDate == "")){
-			alert("All fields should be filled!");
-			return false;
-		}
-		if(!name.match(letters)) {
-			alert("Not a valid name!");
-			return false;
-		}
-		if((!cardNumber1.match(numbers)) || (!cardNumber2.match(numbers)) || (!cardNumber3.match(numbers)) || (!cardNumber4.match(numbers))) {
-			alert("Not a valid credit card number!");
-			return false;
-		}
-		if(!cvv.match(numbers)) {
-			alert("Not a valid cvv number!");
-			return false;
-		}
-	}
 </script>
 </head>
 <body>
--- src/com/ams/controller/TicketController.java
+++ src/com/ams/controller/TicketController.java
@@ -1,5 +1,8 @@
 package com.ams.controller;
 
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -8,11 +11,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.domain.Customer;
+import com.domain.Flight;
+import com.domain.Journey;
+import com.domain.Reservation;
 import com.domain.Traveller;
 import com.service.AuthenticationServiceProxy;
 import com.service.CustomerServiceProxy;
 import com.service.EmployeeServiceProxy;
 import com.service.FlightServiceProxy;
+import com.service.ReservationServiceProxy;
 
 @Controller
 public class TicketController {
@@ -22,10 +30,71 @@ public class TicketController {
 	CustomerServiceProxy custProxy = new CustomerServiceProxy();
 	EmployeeServiceProxy empProxy = new EmployeeServiceProxy();
 	FlightServiceProxy flightProxy = new FlightServiceProxy();
+	ReservationServiceProxy reservationProxy = new ReservationServiceProxy();
 
 	@RequestMapping(value = "/issueTicket.htm", method = RequestMethod.GET)
-	public ModelAndView issueTicket() {
-		return new ModelAndView("issueTicket");
+	public ModelAndView issueTicket(HttpSession session) {
+		
+		ModelAndView mav = new ModelAndView("issueTicket");
+		mav.addObject("res", (Reservation) session.getAttribute("res"));
+		return mav;
+	}
+	
+	@RequestMapping(value = "/issueTicket.htm", method = RequestMethod.POST)
+	public ModelAndView makeResv(@RequestParam("name") String name, 
+			@RequestParam("card") String card,
+			@RequestParam("cvv") Integer cvv,
+			@RequestParam("expiryDate") String expiryDate,
+			HttpSession session) {
+		reservationProxy.setEndpoint("http://localhost:8080/AMS/services/ReservationService");
+		
+		Reservation res = new Reservation();
+		Traveller trav1 = (Traveller) session.getAttribute("traveller1");
+		Traveller trav2 = (Traveller) session.getAttribute("traveller2");
+		Traveller trav3 = (Traveller) session.getAttribute("traveller3");
+		ArrayList<Traveller> travList = new ArrayList<Traveller>();
+		travList.add(trav1);
+		if(trav2 != null)
+			travList.add(trav2);
+		if(trav3 != null)
+			travList.add(trav3);
+		Traveller[] travArray = new Traveller[3];
+		travList.toArray(travArray);
+		res.setTravellers(travArray);
+		
+		res.setSeatsBooked(travList.size());
+		Customer cust = (Customer) session.getAttribute("customer");
+		res.setCustomerId(cust.getCustomerId());
+		
+		Flight flight1 = (Flight) session.getAttribute("flight1");
+		Flight flight2 = (Flight) session.getAttribute("flight2");
+		Journey journey1 = new Journey();
+		journey1.setDateTime(flight1.getFlightTime()[0].getFlightTime());
+		journey1.setDestination((flight1.getDestination()));
+		journey1.setFlightId(flight1.getFlightId());
+		journey1.setSource(flight1.getSource());
+		Journey journey2 = new Journey();
+		journey2.setDateTime(flight2.getFlightTime()[1].getFlightTime());
+		journey2.setDestination((flight2.getDestination()));
+		journey2.setFlightId(flight2.getFlightId());
+		journey2.setSource(flight2.getSource());
+		ArrayList<Journey> journeyList = new ArrayList<Journey>();
+		journeyList.add(journey1);
+		journeyList.add(journey2);
+		
+		Journey[] journeyArray = new Journey[2];
+		journeyList.toArray(journeyArray);
+		res.setJourney(journeyArray);
+		res.setReservationStatus(1);
+		
+		try {
+			reservationProxy.insertReservation(res);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		session.setAttribute("res", res);
+		ModelAndView mav = new ModelAndView("issueTicket");
+		return mav;
 	}
 	
 	@RequestMapping(value = "/showPayment.htm", method = RequestMethod.GET)
