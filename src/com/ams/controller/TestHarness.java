5cb624fce7509a91d1a4d27471ffccbd790e04ce
amitsprofile <amit.agrawal@sjsu.edu>
2013-12-08 20:37:23 -0800

    updated

--- src/com/ams/controller/TestHarness.java
+++ src/com/ams/controller/TestHarness.java
@@ -7,17 +7,20 @@ import java.util.List;
 import java.util.Random;
 
 
-
 import com.domain.Customer;
 import com.domain.Employee;
 import com.domain.Flight;
 import com.domain.FlightTime;
-
+import com.domain.Journey;
+import com.domain.Location;
 import com.domain.Person;
+import com.domain.Reservation;
+import com.domain.Traveller;
 import com.service.AuthenticationServiceProxy;
 import com.service.CustomerServiceProxy;
 import com.service.EmployeeServiceProxy;
 import com.service.FlightServiceProxy;
+import com.service.ReservationServiceProxy;
 
 
 
@@ -27,6 +30,7 @@ public class TestHarness {
 	CustomerServiceProxy custProxy = new CustomerServiceProxy();
 	EmployeeServiceProxy empProxy = new EmployeeServiceProxy();
 	FlightServiceProxy flightProxy = new FlightServiceProxy();
+	ReservationServiceProxy reservationProxy = new ReservationServiceProxy();
 	
 	/*This function generated an array of random numbers which are unique and are between 1 and 5000*/
 	public List<Integer> randomNumberGenerator(int startInt, int endInt)
@@ -101,9 +105,19 @@ public class TestHarness {
 	public boolean testingFlightCreation()
 	{	
 		//PDBConnection dbcon = new PDBConnection();
+		Location[] locations = null; 
 		Random rnd = new Random();
-		//Location[] locations = dbcon.retriveLocations();
-		
+		try {
+			locations = flightProxy.getLocations();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(locations == null)
+		{
+			System.out.println("locations empty");
+			return false;
+		}
 		List<Integer> arrOfInt = randomNumberGenerator(10000,11000);
 		Flight flight;
 		
@@ -114,12 +128,12 @@ public class TestHarness {
 			flight.setFlightId(arrOfInt.get(i));
 			flight.setFlightNo("AF"+i);
 			
-			//int lr = rnd.nextInt(locations.length-1);
-			//flight.setSource(locations[lr].getAirportCode());
-			//int lr1 = rnd.nextInt(locations.length-1);
-			//if(lr == lr1)
-			//	lr1 = rnd.nextInt(locations.length-1);
-			//flight.setDestination(locations[lr1].getAirportCode());
+			int lr = rnd.nextInt(locations.length-1);
+			flight.setSource(locations[lr].getAirportCode());
+			int lr1 = rnd.nextInt(locations.length-1);
+			if(lr == lr1)
+				lr1 = rnd.nextInt(locations.length-1);
+			flight.setDestination(locations[lr1].getAirportCode());
 			flight.setNoOfSeats(100);
 			flight.setAirlineName("AMS");
 			
@@ -144,11 +158,67 @@ public class TestHarness {
 			}
 			
 			flight.setFlightTime(flightTime);
-			//bool = dbcon.createFlight(flight);
+			try {
+				bool = flightProxy.insertFlight(flight);
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return bool;
 	}
 	
+	public boolean testingReservationCreation()/****** currently not working*/
+	{
+		List<Integer> arrOfInt = randomNumberGenerator(20000,30000);
+		Reservation reservation;
+		Traveller []traveller = new Traveller[3];
+		//PDBConnection dbcon = new PDBConnection(); 
+		Journey []journey = new Journey[1];
+		int customerId;
+		for(int j=0; j<3; j++)
+		{
+			traveller[j] = new Traveller();
+			traveller[j].setAge(30);
+			traveller[j].setFirstName("Kanika");
+			traveller[j].setLastName("anand");
+			traveller[j].setSex("F");
+//			traveller[j].setTravellerId();
+		}
+		for(int k=0; k<1; k++)
+		{
+			journey[k] = new Journey();
+			journey[k].setSource("SJC");
+			journey[k].setDestination("AUS");
+			journey[k].setFlightId(1);
+			journey[k].setDateTime("09:00:00");
+			
+		}
+		int count = 0;
+		for(int i=0; i<1000; i++)
+		{
+			customerId = i-10000;
+			reservation = new Reservation();
+			reservation.setCustomerId(customerId);
+			reservation.setReservationId(arrOfInt.get(i));
+			reservation.setReservationNo("anynumber");
+			reservation.setReservationStatus(1);
+			reservation.setSeatsBooked(2);
+			reservation.setTravellers(traveller);
+			reservation.setJourney(journey);
+			try {
+				if(reservationProxy.insertReservation(reservation))
+					count++;
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(count == 1000)
+			return true;
+		else 
+			return false;
+	}
 	public Customer createCustomer(int customerId)
 	{
 
